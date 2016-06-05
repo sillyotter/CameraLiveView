34b05d8fbcc8f3f2c300762c5ffb67259e6c5832
sillyotter <guy.oliver@gmail.com>
2016-06-05 07:57:17 -0400

    one more unit test to confirm something.

--- UnitTests/JpegParserTests.cs
+++ UnitTests/JpegParserTests.cs
@@ -23,7 +23,7 @@ namespace UnitTests
         [Fact]
         public async void ReturnsNullWhenNoJpegFound()
         {
-            using (var ms = new MemoryStream(Enumerable.Range(0, 1000).Select(x => (byte)x).ToArray()))
+            using (var ms = new MemoryStream(Enumerable.Range(0, 1000).Select(x => (byte) x).ToArray()))
             using (var jr = new JpegStreamReader(ms, new MemoryProvider()))
             {
                 var j = await jr.ReadJpegBytesAsync(CancellationToken.None);
@@ -31,13 +31,13 @@ namespace UnitTests
             }
         }
 
-        private static readonly byte[] JpegHeader = { 0xFF, 0xD8, 0xFF };
-        private static readonly byte[] JpegFooter = { 0xFF, 0xD9 };
+        private static readonly byte[] JpegHeader = {0xFF, 0xD8, 0xFF};
+        private static readonly byte[] JpegFooter = {0xFF, 0xD9};
 
         [Fact]
         public async void CanFindOneJpeg()
         {
-            var bufWithJpegInIt = 
+            var bufWithJpegInIt =
                 Enumerable.Range(0, 1000).Select(x => (byte) x)
                     .Concat(JpegHeader)
                     .Concat(Enumerable.Range(0, 1000).Select(x => (byte) x))
@@ -60,15 +60,15 @@ namespace UnitTests
         public async void CanFindTwoJpeg()
         {
             var bufWith2JpegInIt =
-                Enumerable.Range(0, 1000).Select(x => (byte)x)
+                Enumerable.Range(0, 1000).Select(x => (byte) x)
                     .Concat(JpegHeader)
-                    .Concat(Enumerable.Repeat(0xF, 1000).Select(x => (byte)x))
+                    .Concat(Enumerable.Repeat(0xF, 1000).Select(x => (byte) x))
                     .Concat(JpegFooter)
-                    .Concat(Enumerable.Range(0, 1000).Select(x => (byte)x))
+                    .Concat(Enumerable.Range(0, 1000).Select(x => (byte) x))
                     .Concat(JpegHeader)
-                    .Concat(Enumerable.Repeat(0xA, 1000).Select(x => (byte)x))
+                    .Concat(Enumerable.Repeat(0xA, 1000).Select(x => (byte) x))
                     .Concat(JpegFooter)
-                    .Concat(Enumerable.Range(0, 1000).Select(x => (byte)x))
+                    .Concat(Enumerable.Range(0, 1000).Select(x => (byte) x))
                     .ToArray();
 
             using (var ms = new MemoryStream(bufWith2JpegInIt))
@@ -87,6 +87,51 @@ namespace UnitTests
                 Assert.Equal(JpegHeader, j.Item1.Take(3));
                 Assert.Equal(JpegFooter, j.Item1.Skip(1003).Take(2));
                 Assert.True(j.Item1.Skip(3).Take(1000).All(x => x == 0xA));
+            }
+        }
+
+        [Fact]
+        public async void CanFindThreeJpeg()
+        {
+            var bufWith2JpegInIt =
+                Enumerable.Range(0, 1000).Select(x => (byte) x)
+                    .Concat(JpegHeader)
+                    .Concat(Enumerable.Repeat(0xF, 1000).Select(x => (byte) x))
+                    .Concat(JpegFooter)
+                    .Concat(Enumerable.Range(0, 1000).Select(x => (byte) x))
+                    .Concat(JpegHeader)
+                    .Concat(Enumerable.Repeat(0xA, 1000).Select(x => (byte) x))
+                    .Concat(JpegFooter)
+                    .Concat(Enumerable.Range(0, 1000).Select(x => (byte) x))
+                    .Concat(JpegHeader)
+                    .Concat(Enumerable.Repeat(0xB, 1000).Select(x => (byte) x))
+                    .Concat(JpegFooter)
+                    .Concat(Enumerable.Range(0, 1000).Select(x => (byte) x))
+                    .ToArray();
+
+            using (var ms = new MemoryStream(bufWith2JpegInIt))
+            using (var jr = new JpegStreamReader(ms, new MemoryProvider()))
+            {
+                var j = await jr.ReadJpegBytesAsync(CancellationToken.None);
+                Assert.NotEqual(null, j);
+                Assert.Equal(1005, j.Item2);
+                Assert.Equal(JpegHeader, j.Item1.Take(3));
+                Assert.Equal(JpegFooter, j.Item1.Skip(1003).Take(2));
+                Assert.True(j.Item1.Skip(3).Take(1000).All(x => x == 0xF));
+
+                j = await jr.ReadJpegBytesAsync(CancellationToken.None);
+                Assert.NotEqual(null, j);
+                Assert.Equal(1005, j.Item2);
+                Assert.Equal(JpegHeader, j.Item1.Take(3));
+                Assert.Equal(JpegFooter, j.Item1.Skip(1003).Take(2));
+                Assert.True(j.Item1.Skip(3).Take(1000).All(x => x == 0xA));
+
+                j = await jr.ReadJpegBytesAsync(CancellationToken.None);
+                Assert.NotEqual(null, j);
+                Assert.Equal(1005, j.Item2);
+                Assert.Equal(JpegHeader, j.Item1.Take(3));
+                Assert.Equal(JpegFooter, j.Item1.Skip(1003).Take(2));
+                Assert.True(j.Item1.Skip(3).Take(1000).All(x => x == 0xB));
             }
         }
     }
